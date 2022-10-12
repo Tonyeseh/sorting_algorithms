70d8c56ca43ff132830afe2cd77d8515521ff52a
tony <tony@git.com>
2022-10-11 19:26:55 -0700

    added NULL checker to bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -14,6 +14,9 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 
+	if (!array || size < 2)
+		return;
+
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < (size - i - 1); j++)
@@ -23,6 +26,7 @@ void bubble_sort(int *array, size_t size)
 				array[j] += array[j + 1];
 				array[j + 1] = array[j] - array[j + 1];
 				array[j] -= array[j + 1];
+				print_array(array, size);
 			}
 		}
 	}
